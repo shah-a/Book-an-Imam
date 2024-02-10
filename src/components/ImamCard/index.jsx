12043bed99302125109c0c935f4e215b193a4967
Ali Shah <s.ashah@outlook.com>
2024-02-10 07:56:32 -0500

    [Update] adjusted spacing

--- src/components/ImamCard/index.jsx
+++ src/components/ImamCard/index.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line react/prop-types
 function ImamCard({ name, image, description }) {
   return (
-    <div className="ImamCard flex items-center bg-stone-800 shadow-lg rounded-lg overflow-hidden max-w-80">
+    <div className="ImamCard flex items-center bg-stone-800 shadow-lg rounded-lg overflow-hidden w-full">
       <img src={image} alt={name} className="w-24 h-24 rounded-full object-cover ml-4" />
       <div className="py-4 px-6">
         <div className="font-bold text-xl mb-2">{name}</div>
